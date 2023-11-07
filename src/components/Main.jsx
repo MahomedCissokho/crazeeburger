a6a423efd521aab66c61125ab7ef86d4b382d8f0
mahomedcissokho <mahomedcissokho@esp.sn>
2023-11-07 13:12:48 +0100

    updating Navbar component and RightContainer

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,50 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const LeftContainer = styled.div`
+  flex: 1; 
+`
+const MiddleLeftContainer = styled.div`
+  overflow-y : scroll ;
+`
+const RightContainer = styled.div`
+  flex: 3; 
+  overflow-y: scroll;
+`
+const BottomLeftContainer = styled.div`
+  cursor:pointer;
+  font-weight : 600 ;
+  background-color: #ff9f1d;
+  letter-spacing: 1px;
+  &:hover{
+    background-color: white;
+    color : #ff9f1d;
+    transition : all .4s linear ;
+  }
+  border-radius: 7px;
+  width: 45%;
+  
+
+`
 
 const Main = () => {
   return (
-    <div className='text-white -translate-y-32'>
-        sza^dlepldeplde
+    <div className='flex text-white w-screen h-screen -translate-y-[32.3rem]'>
+        <LeftContainer className="bg-[#ccc] flex flex-col justify-between shadow-md">
+            <div className='flex text-xl gap-5 font-semibold bg-neutral-700 h-[4.4rem] items-center justify-between p-7'>
+              <span>Total</span>
+              <span className='text-[#ff9f1b]'>Price</span>
+            </div>
+            <MiddleLeftContainer className='h-[24rem] -translate-y-[2.6rem]'></MiddleLeftContainer>
+            <div className='bg-neutral-700 h-16 flex items-center justify-center -translate-y-20'>
+              <BottomLeftContainer className='hover:bg-white text-center text-lg shadow-2xl flex items-self justify-center py-3'>
+                Payer
+              </BottomLeftContainer>
+            </div>
+        </LeftContainer>
+        <RightContainer className='bg-orange-300 h-[32.4rem] opacity-40	shadow-2xl text-white'>
+
+        </RightContainer>
     </div>
   )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,26 @@
 
-import React from 'react'
+import React, {useState} from 'react'
 import pattern from "../assets/pattern-burger.png";
 import logo from "../assets/logo-orange.png";
 import { UserButton } from "@clerk/clerk-react";
+import ReactSwitch from 'react-switch'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const NavBar = () => {
+
+    const [checked, setChecked] = useState(false)
+    
+    const checkedNotify = () =>toast("Mode Admin activé")
+    const unCheckedNotify = () =>toast("Mode Admin desactivé")
+
+    const handleChange = (val) => {
+        setChecked(val)
+        val ? checkedNotify() : unCheckedNotify()
+    }
+
+
   return (
     <div>
         <div className="w-screen h-screen  -z-50 flex flex-row flex-wrap gap-5 bg-[#fc980c] relative">
@@ -19,13 +35,30 @@ const NavBar = () => {
         <img src={pattern} alt="pattern" className="w-64 h-64" />
         <img src={pattern} alt="pattern" className="w-64 h-64" />
       </div>
+      
       <div className=" opacity-80 top-0 bottom-0 shadow-lg flex right-0 left-0 self-center justify-self-center absolute bg-[#1a1a1a] ">
-        <div className="w-screen h-20 flex items-center justify-between p-10 border-b-2 border-[#ff9f1b]">
+        <div className="w-screen h-20 flex items-center justify-between p-10 border-b-2 border-[#ff9f1b] shadow-inner">
           <div className="cursor-pointer text-[#ff9f1b] text-[2rem] font-semibold flex items-center justify-center gap-4 top-0">
             <span>Crazee</span>
             <img src={logo} alt="logo" className="w-16 h-16 object-cover" />
             <span>Burger</span>
           </div>
+          <ReactSwitch checked={checked} onChange={handleChange}
+            offColor="#ccc"
+            onColor ="#ff9f1b"
+          />
+          <ToastContainer 
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="light"
+          />
           <UserButton />
         </div>
       </div>
